da7801f2d01a2bbb89f85b975ab5baa2988cea18
Winter_ <Nitithorn9581@gmail.com>
2023-08-30 11:12:01 +0700

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import './App.css'
+
+
+//compornents
+import Header from './compornents/Header/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-    
+      <Router>
+        <Header />
+      </Router>
     </div>
   )
 }
